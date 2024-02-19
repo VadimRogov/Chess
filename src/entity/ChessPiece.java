3d2c53e832f85577b9fba72b34990ae3479bea39
Bear <world.life.destiny@icloud.com>
2024-02-19 15:34:00 +0300

    rev1

--- src/entity/ChessPiece.java
+++ src/entity/ChessPiece.java
@@ -1,21 +1,14 @@
 package entity;
 
 public abstract class ChessPiece {
-    private String color;
-    private boolean check;
-
+    String color;
+    boolean check = true;
 
     public ChessPiece(String color) {
         this.color = color;
     }
-    // возвращающий строку — должен вернуть цвет фигуры;
+
     public abstract String getColor();
-
-    // возвращающий логическое (boolean) значение и принимающий в себя параметры
     public abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
-
-    //  возвращающий строку — тип фигуры
     public abstract String getSymbol();
-
-
 }
